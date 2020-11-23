d7ae5cae2297b4eaf428a00e329332dd7a6ec53d
PJ-VDB <pjvandenbrande@hotmail.com>
2020-11-23 10:11:36 +0100

    add port for glitch

--- Module 3/The Weather Here Project/index.js
+++ Module 3/The Weather Here Project/index.js
@@ -7,8 +7,9 @@ require('dotenv').config();
 const express = require('express');
 const DataStore = require('nedb');
 const app = express();
+const port = process.env.PORT || 3000;
 const fetch = require('node-fetch');
-app.listen(3000, () => console.log('listening at 3000'));
+app.listen(port, () => console.log(`Starting server at ${port}`));
 app.use(express.static('public'));
 app.use(express.json({
     limit: '1mb'
